28167455845c610a335f5bec32a4c4582648169e
viorel.tacliciu <viorel.tacliciu@teamnet.ro>
2017-10-17 18:34:20 +0300

    Am redenumit fisierul

--- src/main/java/org/madalin/ierahie1/HelloWorld.java
+++ src/main/java/org/madalin/ierahie1/HelloWorld.java
@@ -1,6 +1,6 @@
 package org.madalin.ierahie1;
 
-public class Ierarhie1 {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
