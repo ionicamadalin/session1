d6efb2522aadebb684cefc677f1a893bd89fb37f
Madalin Ionica <ionicamadalin@gmail.com>
2017-10-17 20:40:25 +0300

    Test. Redunumire clasa.

--- src/main/java/org/madalin/ierahie1/PrimulProgram.java
+++ src/main/java/org/madalin/ierahie1/PrimulProgram.java
@@ -1,7 +1,9 @@
 package org.madalin.ierahie1;
 
-public class HelloWorld {
+public class PrimulProgram {
     public static void main(String[] args) {
+
+
         System.out.println("Hello world!");
     }
 }
