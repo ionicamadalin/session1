98ddbd2589da6eef6c60c4fb1440fd575f0de80f
Madalin Ionica <ionicamadalin@gmail.com>
2017-10-21 14:39:57 +0300

    Am creat clasa Leu ce mosteneste clasa Animal

---
+++ src/main/java/org/madalin/ierahie1/Leu.java
@@ -0,0 +1,10 @@
+package org.madalin.ierahie1;
+
+public class Leu extends Mamifer {
+    String coama;
+
+    public void atacaAnimale(){
+
+        System.out.println("Leul ataca alte animale pentru a se apara");
+    }
+}
