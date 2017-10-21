9a4ecf857c0110568be7b2f9686c97c6594c8a4b
Madalin Ionica <ionicamadalin@gmail.com>
2017-10-21 14:21:52 +0300

    Am creat clasa Mamifer ce mosteneste clasa Animal.

---
+++ src/main/java/org/madalin/ierahie1/Mamifer.java
@@ -0,0 +1,12 @@
+package org.madalin.ierahie1;
+
+public class Mamifer extends Animal {
+
+    String sange_cald;
+
+    protected void seReproduce(){
+
+
+        System.out.println("Mamiferul se reproduce");
+    }
+}
