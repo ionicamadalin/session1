2e39ce05e0d3c4696653a0f5a55d859971620ad1
Madalin Ionica <ionicamadalin@gmail.com>
2017-10-21 15:00:14 +0300

    Am creat clasa Crocodil ce mosteneste clasa Reptila

---
+++ src/main/java/org/madalin/ierahie1/Crocodil.java
@@ -0,0 +1,12 @@
+package org.madalin.ierahie1;
+
+public class Crocodil extends Reptila {
+
+    int lungime;
+
+    public void atacaOameni(){
+
+        System.out.println("Crocodilul ataca oameni");
+
+    }
+}
