d0fa5e31abf19b9aae4585102037a3e8c9bea27c
Madalin Ionica <ionicamadalin@gmail.com>
2017-10-21 14:12:22 +0300

    Test. Redunumire clasa.

---
+++ src/main/java/org/madalin/ierahie1/Animal.java
@@ -0,0 +1,17 @@
+package org.madalin.ierahie1;
+
+import java.util.StringTokenizer;
+
+public abstract class Animal {
+
+        int inaltime;
+        double varsta;
+        String denumire_stiintifica;
+        double greutate;
+        
+        protected void mananca(){
+                
+                System.out.println("Toate animalele pot manca");
+                
+        }
+        }
