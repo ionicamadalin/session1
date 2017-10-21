f1ce51496795f702571742d332fbbb68c5eca119
Madalin Ionica <ionicamadalin@gmail.com>
2017-10-21 14:54:47 +0300

    Am creat clasa Reptila ce mosteneste clasa Animal

---
+++ src/main/java/org/madalin/ierahie1/Reptila.java
@@ -0,0 +1,13 @@
+package org.madalin.ierahie1;
+
+public class Reptila extends Animal {
+    String sangeRece;
+
+    public void setSangeRece(String sangeRece) {
+        this.sangeRece = sangeRece;
+    }
+    public void faceCeva(){
+
+        System.out.println("Reptila face ceva...");
+    }
+}
