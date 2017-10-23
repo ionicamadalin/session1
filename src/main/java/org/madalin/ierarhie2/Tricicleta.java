b55f336fad80556718e0ecb875ce25b4751ca5e7
Madalin Ionica <ionicamadalin@gmail.com>
2017-10-23 22:10:35 +0300

    Am creat clasa Tricicleta ce mosteneste clasa Bicicleta

---
+++ src/main/java/org/madalin/ierarhie2/Tricicleta.java
@@ -0,0 +1,14 @@
+package org.madalin.ierarhie2;
+
+public class Tricicleta extends Bicicleta implements VehiculcuRoti {
+
+    protected String areOroatainplus;
+
+    @Override
+    public void seDeplaseaza() {
+        super.seDeplaseaza();
+    }
+
+    public void mergepestrada(){}
+    public void mergepedrum(){}
+}
