2b29e9d68656464c6e9b58e971302d8249fabb14
Madalin Ionica <ionicamadalin@gmail.com>
2017-10-23 22:26:51 +0300

    Am creat ierarhia 3 cu clasa Telefon

---
+++ src/main/java/org/madalin/ieharhie3/Telefon.java
@@ -0,0 +1,17 @@
+package org.madalin.ieharhie3;
+
+public class Telefon {
+    protected int numar;
+
+    Telefon(){}
+  public void suna(){
+
+        System.out.println("Telefonul poate suna");
+  }
+
+  public void poateComunica(){
+      System.out.println("Orice telefon poate comunica") ;// pur pentru exemplul dat
+    }
+
+
+}
