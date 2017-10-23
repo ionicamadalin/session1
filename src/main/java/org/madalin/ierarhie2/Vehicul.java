cdba58a4ab6cabce41c4c7cbcc45bc2590fb8ea1
Madalin Ionica <ionicamadalin@gmail.com>
2017-10-23 21:09:31 +0300

    Am adugat clasa Vehicul

---
+++ src/main/java/org/madalin/ierarhie2/Vehicul.java
@@ -0,0 +1,17 @@
+package org.madalin.ierarhie2;
+
+public class Vehicul{
+
+    protected String areForma;
+    protected String areCuloare;
+    public void seDeplaseaza(){
+
+        System.out.println("Vehiculul se deplaseaza");
+
+    }
+
+    public void claxoneaza(){
+
+        System.out.println("Orice vehicul poate sa claxoneze");
+    }
+}
